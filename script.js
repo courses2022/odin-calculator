f1517cc369d489a49cec88918f6ee49c38a10ed7
rajpranz <rajeshwarrprasad@yahoo.com>
2022-05-25 02:41:16 +1200

    Add event to buttons

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
+const ADD = "add";
+const DIVIDE = "divide";
+const SUBTRACT = "subtract";
+const MULTIPLY = "multiply";
+var currentOperation = null;
+var total = 0;
+
 var display = document.querySelector('.display');
 
+
+function clearDisplay(){
+    display.innerHTML = "";
+    total =0;
+}
+
+
+function inputNum(n){
+    display.innerHTML=display.innerHTML+n;
+    console.log(n);
+}
+
+function selectOperation(val){
+    currentOperation = val;
+    if(display.innerHTML){
+        total = parseFloat(display.innerHTML);
+        display.innerHTML=""
+    }
+    
+}
+
+
+function findResult(){
+    var currentVal =  parseFloat(display.innerHTML);
+    if (currentOperation == ADD) {
+        total+=currentVal;    
+    } else if(currentOperation==SUBTRACT){
+        total-=currentVal;
+    }else if(currentOperation == MULTIPLY){
+        total*=currentVal;
+    }else if(currentOperation == DIVIDE){
+        total/=currentVal;
+    }else{
+        display.innerHTML="err";
+    }
+
+    display.innerHTML=total;
+    
+}
